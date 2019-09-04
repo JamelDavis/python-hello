e39b8c70a68a948f3cc55919581ff4f0c5467ff3
JamelDavis <48746625+JamelDavis@users.noreply.github.com>
2019-09-04 13:10:35 -0400

    init 1

---
+++ basic.py
@@ -0,0 +1,17 @@
+student_grades = [9.1, 8.8, 7.5]
+
+mysum = sum(student_grades)
+length = len(student_grades)
+mean = mysum / length
+print(mean)
+
+# list count
+# The list.count(10.0) method counts how many 10.0 values are in list.
+student_grades = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9]
+print(student_grades.count(10))
+
+# convert string to lowercase
+# username = "Python3"
+# The str.lower() method converts any string into lowercase letters.
+username = "Python3"
+print(username.lower())
